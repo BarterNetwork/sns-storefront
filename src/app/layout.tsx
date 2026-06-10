430fbca5803f8311d9a09fa331f5cecb0355e9e9
Barter Network <dev@barternetwork.local>
2026-06-10 13:04:02 -0500

    Add Facebook domain verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="facebook-domain-verification" content="dgks0x65cipiwfkol78wzthzxexvq2" />
         {/* Meta Pixel */}
         <script
           dangerouslySetInnerHTML={{
