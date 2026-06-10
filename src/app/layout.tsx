dad41fe98352f356615a60f8bb4f5eac75751607
Barter Network <dev@barternetwork.local>
2026-06-10 12:19:00 -0500

    Add Meta Pixel and fix site title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Threadworks | Custom Apparel & Barter",
+  title: "T-Shirt Depot & More | Wholesale Apparel",
   description:
-    "Premium wholesale activewear and custom t-shirts. Trade what you have for what you need.",
+    "Premium wholesale activewear and custom t-shirts, priced in Barter Bucks.",
 };
 
 export default function RootLayout({
@@ -18,6 +18,28 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
+        />
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '231835949846728');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=231835949846728&ev=PageView&noscript=1" />`,
+          }}
         />
       </head>
       <body>{children}</body>
